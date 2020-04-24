521e2d1c576cc708535c6531538490d96f9a39dd
Matan Ophir <matanophr1@gmail.com>
2020-04-25 02:21:07 +0300

    mid work

---
+++ src/components/Postform.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+export default class Postform extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            title:'',
+            body:'',
+        }
+        this.onChange=this.onChange.bind(this)
+        this.onSubmit=this.onSubmit.bind(this)
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+    
+    onSubmit(e){
+        e.preventDefault();
+
+        const post={
+            title:this.state.title,
+            body:this.state.body
+        }
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+            method:'Post',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              },
+            body:JSON.stringify(post)})
+            .then(res=>res.json())
+            .then(data=>console.log(data))
+
+        
+    }
+    render() {
+        return (
+            <div>
+                <h1>Add Post</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Title:</label><hr/>                        
+                        <input type='text' name='title' onChange={this.onChange} value={this.state.title}/>
+                    </div>
+                    <br/>
+                    <div>
+                        <label>Body:</label><hr/>
+                        <textarea name='body'  onChange={this.onChange} value={this.state.body}/>
+                    </div>
+                    <br/>
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
